10070322d7216b97e2a32e4a67ed70227dabc879
mishrapratij <mishrapratij@gmail.com>
2026-03-02 13:35:20 +0530

    Day 24 - Delete First Occurrence of a Key

---
+++ Q1DAy24.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* insertEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+struct Node* deleteKey(struct Node* head, int key) {
+    if (head == NULL)
+        return NULL;
+
+    if (head->data == key) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    struct Node* current = head;
+    while (current->next != NULL && current->next->data != key) {
+        current = current->next;
+    }
+
+    if (current->next != NULL) {
+        struct Node* temp = current->next;
+        current->next = temp->next;
+        free(temp);
+    }
+
+    return head;
+}
+
+int main() {
+    int n, value, key;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head = insertEnd(head, value);
+    }
+
+    scanf("%d", &key);
+
+    head = deleteKey(head, key);
+
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
