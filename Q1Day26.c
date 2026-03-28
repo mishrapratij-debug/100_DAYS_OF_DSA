6093a699e24b77d1908309c039fd5685b5874bfe
mishrapratij <mishrapratij@gmail.com>
2026-03-28 22:06:33 +0530

    Day 26 -Doubly Linked List Insertion and Traversal

--- Q1Day26.c
+++ Q1Day26.c
@@ -9,6 +9,7 @@ struct Node {
 
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) exit(1);
     newNode->data = data;
     newNode->prev = NULL;
     newNode->next = NULL;
