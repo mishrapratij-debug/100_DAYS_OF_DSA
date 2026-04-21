98ba892bca5740c78ede25c2b40fe001efa90d9d
mishrapratij <mishrapratij@gmail.com>
2026-04-21 14:33:45 +0530

    Day 31 -Stack Implementation using Array

---
+++ Q1Day31.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) {
+    if (top == MAX - 1)
+        return;
+    stack[++top] = x;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("%d\n", stack[top--]);
+}
+
+void display() {
+    for (int i = top; i >= 0; i--) {
+        printf("%d ", stack[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n, op, value;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &op);
+
+        if (op == 1) {
+            scanf("%d", &value);
+            push(value);
+        } else if (op == 2) {
+            pop();
+        } else if (op == 3) {
+            display();
+        }
+    }
+
+    return 0;
+}
